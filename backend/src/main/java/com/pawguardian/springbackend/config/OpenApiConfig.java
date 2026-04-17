d5fc4e51246e5f70eaef70d4db8c73dc1a811c62
DaniPTRK <idani3093@gmail.com>
2026-04-17 23:17:00 +0300

    Fixed a bug regarding auth

--- backend/src/main/java/com/pawguardian/springbackend/config/OpenApiConfig.java
+++ backend/src/main/java/com/pawguardian/springbackend/config/OpenApiConfig.java
@@ -3,13 +3,18 @@ package com.pawguardian.springbackend.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Authorization server", version = "v1"))
+@OpenAPIDefinition(
+        info = @Info(title = "PawGuardian API", version = "v1"),
+        security = @SecurityRequirement(name = "Bearer Authentication")
+)
+
 @SecurityScheme(
-        name = "bearerAuth",
+        name = "Bearer Authentication",
         type = SecuritySchemeType.HTTP,
         bearerFormat = "JWT",
         scheme = "bearer"
