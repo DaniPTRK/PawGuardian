4e4cf9666391b30f10e52963279a1d4337413c7f
DaniPTRK <idani3093@gmail.com>
2026-04-17 03:29:20 +0300

    Added APIs to manage owner's pets. Added new species of pets

--- backend/src/main/resources/db/migration/V3__Add_Species.sql
+++ backend/src/main/resources/db/migration/V3__Add_Species.sql
@@ -2,5 +2,7 @@ INSERT INTO project.pet_species (id, name)
 VALUES
     (1, 'DOG'),
     (2, 'CAT'),
-    (3, 'OTHER')
+    (3, 'RABBIT'),
+    (4, 'BIRD'),
+    (5, 'OTHER')
 ON CONFLICT (id) DO NOTHING;
