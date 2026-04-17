187c1da9b7755dd9e9457abde8343f633de8f989
Daniel Ion <daniel.ion@keysight.com>
2026-04-17 03:41:34 +0300

    Revert "Added APIs to manage owner's pets. Added new species of pets"
    This reverts commit 8c59293f8a72d9689bb8a7080e5a0fe3fc0f0139.

--- backend/src/main/resources/db/migration/V3__Add_Species.sql
+++ backend/src/main/resources/db/migration/V3__Add_Species.sql
@@ -2,7 +2,5 @@ INSERT INTO project.pet_species (id, name)
 VALUES
     (1, 'DOG'),
     (2, 'CAT'),
-    (3, 'RABBIT'),
-    (4, 'BIRD'),
-    (5, 'OTHER')
+    (3, 'OTHER')
 ON CONFLICT (id) DO NOTHING;
