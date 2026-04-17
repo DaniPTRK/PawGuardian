a1b7a623d243e048c2f96ef8181dabd9488f5001
Daniel Ion <daniel.ion@keysight.com>
2026-04-17 03:41:41 +0300

    Revert "Added DTOs for the pet requests."
    This reverts commit 73cac1d689053e2804dca6136ddf34d0076e20cf.

--- backend/src/main/java/com/pawguardian/springbackend/repository/PetRepository.java
+++ backend/src/main/java/com/pawguardian/springbackend/repository/PetRepository.java
@@ -1,13 +1,4 @@
 package com.pawguardian.springbackend.repository;
 
-import com.pawguardian.springbackend.entity.Pet;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-@Repository
-public interface PetRepository extends JpaRepository<Pet, Long> {
-
-    List<Pet> findAllByOwnerId(Long ownerId);
+public interface PetRepository {
 }
